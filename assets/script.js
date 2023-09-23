72eaebdbe2523dd4ccf3c8ee271cbf2f1603dde4
Kurre Sai lakshma reddy <lakshma120798@gmail.com>
2023-09-23 17:42:22 +0100

    updated

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 function listTopics() {
     for(var x = 0; x < topics.length; x++ ){
         console.log (topics [x]);
-     }
+    }
 }
 
 function selectTopic(){
